459cca1826ccc7cb278e6ac8d32dbac93c3ffef7
chenshiyuhit <chenshiyuhit@gmail.com>
2020-12-28 21:52:50 +0800

    used to get PID controller trajectory position

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-uav_data_get
-============
+Subscribe the topic from UAV mavros and record the data to the `*.txt` file.
 
-Subscribe the topic from UAV mavros and record the data to the *.txt file.
+```
+rosrun uav_data_get position_data
+```
--- src/position_data.cpp
+++ src/position_data.cpp
@@ -1,41 +1,37 @@
 #include <stdio.h>
 
-#include "ros/ros.h"
-#include <geometry_msgs/PoseStamped.h>
-
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 
 float uav_x;
 float uav_y;
 float uav_z;
 
+FILE *position_data_file = fopen("/home/csy/catkin_ws/src/uav_data_get/data/position_data.txt","w");
 
-void localposeCallBack(const geometry_msgs::PoseStampedConstPtr &msg)
+void localposeCallBack(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    uav_x = msg->pose.position.x;
-    uav_y = msg->pose.position.y;
-    uav_z = msg->pose.position.z;
+    uav_x = msg->pose.pose.position.x;
+    uav_y = msg->pose.pose.position.y;
+    uav_z = msg->pose.pose.position.z;
+
+    fprintf(position_data_file,"%f	%f	%f\n",uav_x,uav_y,uav_z);
+	ROS_INFO("%f    %f    %f\n",uav_x,uav_y,uav_z);
 }
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "position_data");
+    ros::Time::init();
+    ros::Rate loop_rate(10);
 
     ros::NodeHandle l_nh_;
     ros::Subscriber localpose_sub_;
 
-    localpose_sub_= l_nh_.subscribe("/firefly/ground_truth/pose",1000,localposeCallBack);
+    // localpose_sub_= l_nh_.subscribe("/hummingbird/ground_truth/odometry",1000,localposeCallBack);
+    localpose_sub_= l_nh_.subscribe("/hummingbird/odometry_sensor1/odometry",1000,localposeCallBack);
 
-	FILE *position_data_file = fopen("/home/csy/catkin_ws/src/uav_data_get/data/position_data.txt","w");
-
-    while(ros::ok())
-    {
-		ros::spinOnce();
-		fprintf(position_data_file,"%f	%f	%f\n",uav_x,uav_y,uav_z);
-		ROS_INFO("%f    %f    %f\n",uav_x,uav_y,uav_z);
-		cv::waitKey(100);
-    }
+    ros::spin();
     
 	fclose(position_data_file);
     return 0;
